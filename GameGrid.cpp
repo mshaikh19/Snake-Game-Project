f127cb086ef39205cc69c44e6e2fa693086e6daa
Anistina-Dsouza <dsouzaanistina@gmail.com>
2025-11-03 20:04:35 +0530

    Changes in gamegrid

--- GameGrid.cpp
+++ GameGrid.cpp
@@ -1,73 +1,35 @@
-#include "GameGrid.h"
+#ifndef GAME_GRID_H
+#define GAME_GRID_H
+
+#include <iostream>
 #include "Snake.h"
+#include "Food.h"
 
-#include <vector>
+class Snake;
 
-using namespace std;
+extern const int gridHeight;
+extern const int gridWidth;
 
-// Grid of NxN is defined
-const int gridHeight = 10;
-const int gridWidth = 20;
-
-// Clear the whole terminal screen for a cleaner look
-void clearScreen(){
-	#ifdef _WIN32
-        system("cls");
-    #endif
-}
-
-// Drawing the Grid along with the initial position of the Snake on the terminal
-void drawGameGrid(const Snake& snake, int score){
-	
-	clearScreen();
-	cout << "Score : " << score << "\t Current Direction : " << snake.getDirection() << endl;
-	
-//	Drawing the Top Boundry
-//	The top left corner boundry
-	cout << "|";
-	
-//	The top boundry
-	for(int i = 0; i < gridWidth; ++i){
-		cout << "-";
-	} 
-	
-//	The top right corner boundry
-	cout << "|";
-	cout << endl;
-	
-//	Drawing the Game Area
-	for(int i = 0; i < gridHeight; i++){
-//		The left boundry
-		cout << "|";
+class GameGrid{
+	private:
+		int width;
+		int height;
 		
-//		The game area
-		for(int j = 0; j < gridWidth; j++){
-			bool isDrawn = false;
-			
-//			Rendering the Snake in the grid
-			for(size_t k = 0; k < snake.getBody().size(); ++k)			
-			
-			cout << ".";
-		}
+		void clearScreen();
 		
-//		The right boundry
-		cout << "|";
+	public:
+		GameGrid(int width, int height);
 		
-		cout << endl;
-	}
-	
-//	Drawing the bottom boundry
+		int getWidth() const;
+		int getHeight() const;
+		
+//		Draw Empty Grid
+		void drawGameGrid();
+		
+//		Draw grid along with the snake and the score
+		void drawGameGrid(const Snake& snake, const Food& food, int score);
+		
+		
+};
 
-//	The bottom left corner boundry
-	cout << "|";
-	
-//	The bottom boundry
-	for(int i = 0; i < gridWidth; ++i){
-		cout << "-";
-	} 
-	
-//	The bottom right corner boundry
-	cout << "|";
-	
-	cout << endl;
-}
+#endif
